387c89fa4dcbef4a72389736e87d7a5fc1f65424
Tristan Deane <59102133+IM-Deane@users.noreply.github.com>
2022-11-07 13:23:43 -0700

    Wrote database layer CRUD handler funcs

--- mdb/mdb.go
+++ mdb/mdb.go
@@ -38,6 +38,7 @@ func TryCreate(db *sql.DB) {
 	}
 }
 
+// emailEntryFromRow build an email entry from provided DB row
 func emailEntryFromRow(row *sql.Rows) (*EmailEntry, error) {
 	var id int64
 	var email string
@@ -55,4 +56,131 @@ func emailEntryFromRow(row *sql.Rows) (*EmailEntry, error) {
 	t := time.Unix(confirmedAt, 0)
 
 	return &EmailEntry{ID: id, Email: email, ConfirmedAt: &t, OptOut: optOut}, nil
+}
+
+
+// CreateEmail adds new entry to email table
+func CreateEmail(db *sql.DB, email string) error {
+	_, err := db.Exec(`
+		INSERT INTO
+			emails(email, confirmed_at, opt_out)
+		VALUES
+			(?, 0, false)`, email)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+
+// GetEmail fetches email entry from DB
+func GetEmail(db *sql.DB, email string) (*EmailEntry, error) {
+	rows, err := db.Query(`
+		SELECT
+			id, email, confirmed_at, opt_out
+		FROM
+			emails
+		WHERE
+			email = ?`, email)
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	// close DB connection if any error occurs
+	defer rows.Close()
+
+	// read new row from DB
+	for rows.Next() {
+		return emailEntryFromRow(rows)
+	}
+
+	return nil, nil
+}
+
+
+// UpdateEmail updates a given email entry or creates a new one if it doesn't exist
+func UpdateEmail(db *sql.DB, entry EmailEntry) error {
+	t := entry.ConfirmedAt.Unix()
+
+	// UPSERT email (try to create new entry, if it exists update instead)
+	_, err := db.Exec(`
+		INSERT INTO
+			emails(email, confirmed_at, opt_out)
+		VALUES
+			(?, ?, ?)
+		ON CONFLICT(EMAIL) DO UPDATE SET
+			confirmed_at=?
+			opt_out=?`, entry.Email, t, entry.OptOut, t, entry.OptOut)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+
+// DeleteEmail soft deletes email from mailing list
+// NOTE: we keep the record to avoid edgecase where we
+// send an email to someone that's already opted out (ie. spam).
+func DeleteEmail(db *sql.DB, email string) error {
+	// setting opt_out=true removes that email from the mailing list
+	_, err := db.Exec(`
+		UPDATE emails
+		SET opt_out=true
+		WHERE email=?`, email)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+type GetEmailBatchQueryParams struct {
+	Page int
+	Count int
+}
+
+// GetEmailBatch fetches all users currently subscribed to mailing list
+func GetEmailBatch(db *sql.DB, params GetEmailBatchQueryParams) ([]EmailEntry, error) {
+	var empty []EmailEntry
+
+	// get current users offset by current page
+	rows, err := db.Query(`
+		SELECT
+			id, email, confirmed_at, opt_out
+		FROM
+			emails
+		WHERE
+			opt_out = false
+		ORDER BY id ASC
+		LIMIT ? OFFSET ?`, params.Count, (params.Page-1)*params.Count)
+
+	if err != nil {
+		log.Println(err)
+		return empty, err
+	}
+
+	// close DB connection on error or end of func
+	defer rows.Close()
+
+	// create slice of email list
+	emails := make([]EmailEntry, 0, params.Count)
+
+	// read rows from DB
+	for rows.Next() {
+		email, err := emailEntryFromRow(rows)
+		if err != nil {
+			// cancel iteration as we don't want a partial list
+			return nil, err
+		}
+		emails = append(emails, *email)
+	}
+
+	return emails, nil
 }
